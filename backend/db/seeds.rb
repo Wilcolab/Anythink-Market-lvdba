48923de56ea5c75122fb4423d5c1c48f232e06ba
jonobarel <jonobarel@users.noreply.github.com>
2022-05-08 11:19:34 +0300

    item creation
    Seeded item
    fixed reference to ruby binary

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+'''
+create_table "items", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.string "slug"
+    t.string "description"
+    t.string "image"
+    t.integer "favorites_count"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_items_on_slug", unique: true
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+  '''
+
+  items_to_load = Item.create([{title: "item1", slug: "item-1", description: "This is the description of the item", image: "", user_id: "1"}])
