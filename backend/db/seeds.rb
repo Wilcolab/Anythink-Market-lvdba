ae0af5a9141623b01f0cba2fa558e7723301e88f
jonobarel <jonobarel@users.noreply.github.com>
2022-10-27 11:34:02 +0300

    Added 100 each of users, items and comments

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_fields = ["email", "encrypted_password", "username"]
+item_fields = ["title", "description", "user_id"]
+comment_fields = ["body", "user_id", "item_id"]
+
+(1...100).each do |i|
+    user = User.create(
+        "email": "user#{i}@address.com",
+        "username": "user#{i}",
+        "password": "123456"
+    )
+    user.save
+    item = Item.create(
+        "title": "item#{i}",
+        "description": "this is a description for item #{i}",
+        "user_id": i
+    )
+    item.save
+    comment = Comment.create(
+        "body": "commend number #{i}",
+        "user_id": i,
+        "item_id": i
+    )
+    comment.save
+end
+
+
+
+
