64b0fb1883734ea82c6f1dc3972631ff87a6e124
jonobarel <jonobarel@users.noreply.github.com>
2022-10-30 16:49:41 +0200

    Revert "Added migration that sets default image to placeholder.png"
    This reverts commit fe32c13f0347ebedc8673fb065ff2db514b49dc7.

--- backend/db/migrate/20221030143001_set_default_value_for_image_in_item.rb
+++
@@ -1,8 +0,0 @@
-class SetDefaultValueForImageInItem < ActiveRecord::Migration[6.1]
-  def up
-    change_column(:items, :image, :string,  :default => "placeholder.png")
-  end
-  def down
-    change_column(:items, :image, :string)
-  end
-end
